04469c80f4406446fdecc04e02081c687176056c
Ilya <zhdilya7@gmail.com>
2025-08-28 02:24:56 +0300

    b

--- practtour2025/header.h
+++ practtour2025/header.h
@@ -18,9 +18,32 @@ struct Student {
 	int32_t mark_prog{};
 };
 
-void FillArrayFromFileStud(std::istream& fin_mark, std::istream& fin_stud, Student* student, int32_t size);
 void CheckInputFile(std::istream& fin);
 
-void FillBinaryFileFromTextFile(std::ifstream& fin_mark, std::fstream& bin_mark);
+int32_t FindCountOfStudents(std::istream& fin);
+void FillStructArrayFromFiles(std::istream& fin_mark, std::istream& fin_stud, Student* student, int32_t size);
 
+
+void FillBinaryFileFromTextFile(std::ifstream& fin_mark, std::fstream& bin_mark);//заполняет бинарный файл из текстового
+
+void SortStructById(Student*& students, int32_t size);//сортирует структуру по id
+
+void FillStructFromFileWithMarks(std::ifstream& fin_mark, Student* student, int32_t size);//заполняет структуру из файла с оценками
+void FillFileWithMark(std::string& filename, Student* students, int32_t size);//заполняет файл с оценками 
+
+void FillStructFileWithFIO(std::ifstream& fin_stud, Student* students, int32_t size);//заполняет структуру из файла с фамилиями
+void FillFileWithFIO(std::string& filename, Student* students, int32_t size);//заполняет файл с фамилиями студентов
+
+void MakeBinaryFileWithSurnAndMarks(Student* students, int32_t size);//создает бинарный файл в папке и записывает фамилии с оценками
 #endif
+
+//4; 456349; МА; 5; ГЕО; 9; ПРОГ; 10
+//3; 456348; МА; 4; ГЕО; 2; ПРОГ; 4
+//4; 456340; МА; 5; ГЕО; 6; ПРОГ; 10
+//1; 456345; МА; 4; ГЕО; 5; ПРОГ; 2
+
+
+//456345; Иванов; Иван; Петрович
+//456348; Петров; Петр; Иванович
+//456340; Иванова; Мария; Семеновна
+//456349; Петрова; Наталья; Егоровна
--- practtour2025/main.cpp
+++ practtour2025/main.cpp
@@ -5,6 +5,10 @@ int main() {
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 
+	std::string fileNameMark = "Group_ID_MA_Mark_GEO_Mark_PROG_Mark.txt";
+	std::string fileNameStud = "ID_SNP.txt";
+
+
 	std::ifstream fin_mark("Group_ID_MA_Mark_GEO_Mark_PROG_Mark.txt");
 	std::ifstream fin_stud("ID_SNP.txt");
 	std::fstream bin_mark("Bin_Group_ID_MA_Mark_GEO_Mark_PROG_Mark.bin", std::ios::in | std::ios::out | std::ios::binary);
@@ -14,9 +18,26 @@ int main() {
 		CheckInputFile(fin_mark);
 		CheckInputFile(fin_stud);
 
+		int32_t size = FindCountOfStudents(fin_mark);
+		Student* students = new Student[size];
+
+		//a
 		FillBinaryFileFromTextFile(fin_mark, bin_mark);
 		FillBinaryFileFromTextFile(fin_stud, bin_stud);
 
+
+		FillStructFromFileWithMarks(fin_mark, students, size);
+		SortStructById(students, size);
+		FillFileWithMark(fileNameMark, students, size);
+
+		FillStructFileWithFIO(fin_stud, students, size);
+		SortStructById(students, size);
+		FillFileWithFIO(fileNameStud, students, size);
+
+		FillStructArrayFromFiles(fin_mark, fin_stud, students, size);
+
+		//b
+		MakeBinaryFileWithSurnAndMarks(students, size);
 
 	}
 	catch (const char* m) {
